6edf9aeeda4934c58e061cd517d990f25ed524b7
Luca Carrozzo <carrozzo.lc@gmail.com>
2021-01-24 22:27:26 +0100

    http app added

---
+++ http-app/README.md
@@ -0,0 +1,8 @@
+# Http request app
+### App built to practice and understand http requests using Axios with React.
+
+How to use the downloaded files
+
+    1) Run "npm install" in the extracted folder
+
+    2) Run "npm start" to view the project
---
+++ http-app/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+import FullPost from '../FullPost/FullPost';
+
+class Posts extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                //console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                //this.setState({error: true})
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        console.log(this.props)
+        this.props.history.push({ pathname: '/posts/' + id });
+        //this.props.history.push( '/posts/' + id );
+    }    
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    // <Link to={'/posts' + post.id} key={post.id}>
+                        <Post
+                            key={post.id}
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link>
+                );       
+            });
+        }
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />       
+            </div>        
+        );
+    }
+};
+
+export default Posts;
